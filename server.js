38aa2ec50bc41dba31ff35d17c9f7a8dc61420ac
Onasanya Tunde <Onasanyatunde67@gmail.com>
2020-08-08 13:41:33 +0100

    Heroku

--- server.js
+++ server.js
@@ -36,10 +36,9 @@ app.use(bodyParser.json());
 
 // Server
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/bulid'));
-  // app.use(express.static(path.join(__dirname, 'client/build')));
+  app.use(express.static('client/build'));
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client/build'));
+    res.sendFile(path.join(`${__dirname}/client/build/index.html`));
   });
 }
 

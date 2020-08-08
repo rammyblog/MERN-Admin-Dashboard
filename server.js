72f2551bf9d83fc7538c44426d08b11f39692a39
Onasanya Tunde <Onasanyatunde67@gmail.com>
2020-08-08 13:33:52 +0100

    Trying to fix heroku bugs

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/bulid'));
   // app.use(express.static(path.join(__dirname, 'client/build')));
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'client/build'));
   });
 }
 
